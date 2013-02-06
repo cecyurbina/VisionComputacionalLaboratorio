2ecb4c3a87d3e4df1d05063db9f4489f8ed40226
Cecilia Urbina <ceciliaurbina29@gmail.com>
2013-02-06 14:38:35 -0600

    [practica_1] agregué comentarios al código

--- practica_1.py
+++ practica_1.py
@@ -48,7 +48,9 @@ class Aplicacion:
         self.picture.grid(row=1, column=0, columnspan=7,sticky=W+E+N+S, padx=5, pady=5)
 
     def abrir_imagen_original(self):
-        """
+        """funcion que se manda llamar para
+        poder usar la imagen que se selecciono
+        al inicio
         """
         imagen = Image.open(self.nombre_imagen)
         imagen = imagen.convert('RGB')
@@ -56,17 +58,19 @@ class Aplicacion:
 
     def reiniciar(self):
         """
+        funcion que regresa la foto al estado inicial
         """
         self.imagen_actual = self.imagen_original
         self.actualizar_imagen()
     
     def guardar(self):
-        """
+        """guarda la imagen con el nombre
+        imagen_nueva.png
         """
         self.imagen_actual.save("imagen_nueva.png")
 
     def actualizar_imagen(self):
-        """
+        """redibuja la foto en la ventana
         """
         foto  = ImageTk.PhotoImage(self.imagen_actual)
         self.picture = Label(self.frame, image=foto)
@@ -74,7 +78,7 @@ class Aplicacion:
         self.picture.grid(row=1, column=0, columnspan=7,sticky=W+E+N+S, padx=5, pady=5)        
 
     def espejo(self):
-        """
+        """cambia el sentido de la imagen
         """
         imagen_espejo = Image.new("RGB", (self.x, self.y))
         pixeles = []
@@ -87,12 +91,14 @@ class Aplicacion:
             renglon.reverse()
             pixeles = pixeles + renglon
         imagen_espejo.putdata(pixeles)
+        #se actualiza la imagen actual y se redibuja
         self.imagen_actual = imagen_espejo
         self.actualizar_imagen()
         return imagen_espejo
 
     def hacer_gris(self):
-        """
+        """pone la foto en escala de grises
+        toma el valor maximo del rgb de cada pixel
         """
         imagen_gris = Image.new("RGB", (self.x, self.y))
         pixeles = []
@@ -105,12 +111,14 @@ class Aplicacion:
                 data = (maximo, maximo, maximo)
                 pixeles.append(data)
         imagen_gris.putdata(pixeles)
+        #se actualiza la imagen actual y se redibuja
         self.imagen_actual = imagen_gris
         self.actualizar_imagen()
         return imagen_gris
 
     def umbral(self):
-        """
+        """binariza la imagen si el valor del pixel es de color menor a 
+        150 se pone 0 en el pixel, si es mayo se pone 255 en el pixel
         """
         imagen_umbral = Image.new("RGB", (self.x, self.y))
         pixeles = []
@@ -129,7 +137,8 @@ class Aplicacion:
         return imagen_umbral
 
     def invertir(self):
-        """
+        """cambia los colores de la imagen a su contrario
+        restando 255 a cada pixel
         """
         imagen_invertida = Image.new("RGB", (self.x, self.y))
         pixeles = []
@@ -140,6 +149,7 @@ class Aplicacion:
                 data = (color, color, color)
                 pixeles.append(data)
         imagen_invertida.putdata(pixeles)
+        #se actualiza la imagen actual y se redibuja
         self.imagen_actual = imagen_invertida
         self.actualizar_imagen()
         return imagen_invertida
@@ -158,6 +168,8 @@ class Aplicacion:
             for b in range(self.x):
                 actual = self.imagen_actual.getpixel((b, a))[0]
                 if b>0 and b<(self.x-1) and a>0 and a<(self.y-1):
+                    #en esta condicion entran todos los pixeles que no estan
+                    #en el margen de la imagen, es decir casi todos
                     pix_izq = self.imagen_actual.getpixel((b-1, a))[0]
                     pix_der = self.imagen_actual.getpixel((b+1, a))[0]
                     pix_arriba = self.imagen_actual.getpixel((b, a+1))[0]
@@ -167,6 +179,7 @@ class Aplicacion:
                     temp.append(pix_arriba)
                     temp.append(pix_abajo)
                 else:
+                    #aqui entran todos los pixeles de la orilla
                     try:
                         pix_abajo = self.imagen_actual.getpixel((b, a-1))[0]
                         temp.append(pix_abajo)
@@ -188,10 +201,12 @@ class Aplicacion:
                     except:
                         pass
                 temp.append(actual)
+                #se obtiene el promedio para cambiar el pixel
                 prom = sum(temp)/len(temp)
                 temp = []
                 pixeles.append((prom, prom, prom))
         imagen_difusa.putdata(pixeles)
+        #se actualiza la imagen actual y se redibuja
         self.imagen_actual = imagen_difusa
         self.actualizar_imagen()
         return imagen_difusa
